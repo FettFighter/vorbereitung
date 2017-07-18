c1f0e71d0b1f4772ffae7a1a20a352d878e23f0b
FettFighter <wowmensch@gmail.com>
2017-07-18 18:18:21 +0200

    Nur test im Starter durchgeführt.

--- EVL.java
+++ EVL.java
@@ -1,4 +1,63 @@
-public class EVL {
+public class EVL<E> {
 
-  private Knoten start;
+  private Knoten<E> start;
+
+  public EVL(){
+    this.start = new Knoten<E>();
+  }
+
+  public EVL(E firstValue) {
+    this.start = new Knoten<E>(firstValue);
 }
+
+  public E firstValue() {
+    return start.anyValue();
+  }
+
+  //Methode: prepend hängt vor das ERSTE Element einen Knoten und macht das ERSTE zum ZWEITEN Element und das neue Element zum ERSTEN
+  public void prepend(Knoten<E> knoten) { 
+    knoten.knotenRechts(start);
+    start = knoten;
+  }
+  //genau wie oben nur mit Wert.
+  public void prependValue(E value) {
+    prepend(new Knoten<E>(value));
+  } 
+
+  public String toString() {
+    String temp = "";
+    Knoten tempKnoten;
+
+    temp += start;
+    tempKnoten = start.knotenRechts();
+
+    while(tempKnoten != null) {
+      temp += tempKnoten;
+      tempKnoten = tempKnoten.knotenRechts();
+    }
+
+    return temp;
+  }
+
+  public int laengeListe() {
+    int index;
+    Knoten tempKnoten;
+    
+    tempKnoten = start.knotenRechts();
+    for(index = 1; tempKnoten != null; index++) {
+      tempKnoten = tempKnoten.knotenRechts();
+    }
+    return index;
+  }
+  
+  public Knoten letzerWert(Knoten<E> letzterWert) {
+    
+    return null;
+  }
+ 
+}
+
+
+// [KLASSE].[ZEIGT WAS STATISCH UND DEN RICHTIGEN ACCESSMODIFIER HAT AN]
+// [INSTANZVARIABLE.[ZEIGT ALLES AN WAS IN DER KLASSE IST MIT DEM ACCESSMODIRIER]
+// KONiSTRUKTOREN WERDEN NUR MIT NEW ANGEZEIGT
--- Starter.java
+++ Starter.java
@@ -7,7 +7,7 @@ public class Starter {
     Paar<Paar<Integer, Integer>, Paar<Integer, Integer>> paar3 = new Paar<Paar<Integer, Integer>, Paar<Integer, Integer>>(paar, paar);
     /*Ein Paar und ein String des Paar */
     Paar<Paar<Integer, Integer>, String> paar4 = new Paar<Paar<Integer, Integer>, String>(paar,paar.toString());
-
+    
     //GleichesPaar
     GleichesPaar<Integer> gleichesPaar = new GleichesPaar<>(paar4.erstes().erstes(),paar.zweites());
 
@@ -16,16 +16,33 @@ public class Starter {
     Knoten<Integer> knoten2 = new Knoten<Integer>(22);
     Knoten<Integer> knoten3 = new Knoten<Integer>();
     Knoten<String> knoten4 = new Knoten<String>("LoL");
+    Knoten<Paar<Integer, Integer>> knoten5 = new Knoten<Paar<Integer, Integer>>(new Paar<Integer, Integer>(3,4));
     
-    //Knoten1) knotenRechts soll auf knoten2 zeigen | knoten2) knotenRechts  auf knoten3 | Auf Referenzen zeigen.
+   // Knoten1) knotenRechts soll auf knoten2 zeigen | knoten2) knotenRechts  auf knoten3 | Auf Referenzen zeigen.
     knoten.knotenRechts(knoten2); 
     knoten2.knotenRechts(knoten3);
     knoten.knotenRechts().knotenRechts().knotenRechts(knoten4);
 
+    //EVL
+    EVL<Integer> evl = new EVL<Integer>(256);
+    EVL<Paar<Integer, Integer>> evl2 = new EVL<Paar<Integer, Integer>>(new Paar<Integer, Integer>(88,99));
+    evl.prepend(knoten2);
+    evl2.prepend(knoten5);
+    evl2.prependValue(new Paar<Integer, Integer>(19,20));
+    System.out.println("EVL EVL EVL EVL");
+    System.out.println(evl.firstValue());
+    System.out.println(evl.toString());
+    System.out.println(evl.laengeListe());
+    System.out.println(evl2.toString());
+    System.out.println(evl2.laengeListe());
+    
+ 
+    System.out.println("KNOTEN KNOTEN KNOTEN KNOTEN");
     System.out.println(knoten.knotenRechts());
     System.out.println(knoten2.knotenRechts());
     System.out.println(knoten.knotenRechts().knotenRechts().knotenRechts());
 
+    System.out.println("PAAR PAAR PAAR PAAR");
     System.out.println(paar.toString());
     System.out.println(paar4.toString());
     System.out.println(paar4.erstes().getClass().toString()+ paar4.zweites().getClass().toString());
